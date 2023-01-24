4c4ff4a13d31a3c306f03088e5fbb0cb3d2413b4
Bishal Dhakal <dhakalbishal930@gmail.com>
2023-01-24 13:30:40 +0600

    small changes made

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,8 @@ class PagesController < ApplicationController
     end
     def about
     end
+    def login
+    end
+    def signup
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root "pages#home"
-  get '/about', to 'pages#about'
+  get '/about', to: 'pages#about'
+  get '/login', to: 'pages#login'
+  get "/signup", to: 'pages#signup'
 end
